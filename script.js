4023b0ccfb337e9f8471ee6fe3ecacf848ac792c
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2023-12-15 16:10:21 -0400

    sidebar finished

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-console.log("Conected");
+const menubarsBtn = document.querySelector('.menu_bars');
+
+menubarsBtn.addEventListener("click", displaySideMenu);
 
 function transformacionX(x)
 {
     x.classList.toggle("change");
+}
+
+function displaySideMenu()
+{
+    const body = document.querySelector('body');
+    const header = document.querySelector('header');
+
+    header.classList.toggle("active");
+    body.classList.toggle("active");
 }
